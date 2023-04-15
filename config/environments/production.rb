52f40ceb2ed4c81f469133d7f5d28d2ba7fb7315
https://bitbucket.org/vijay9926 <patidarvijay590@gmail.com>
2023-04-16 00:26:25 +0530

    render/config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
+  production:
+  <<: *default
+  url: <%= ENV['DATABASE_URL'] %>
 end
