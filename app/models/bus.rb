9f0bf6b61bc8d91bb12eb0d8d83ed8fa3617f01d
https://bitbucket.org/vijay9926 <patidarvijay590@gmail.com>
2023-04-17 18:46:02 +0530

    bin/render-build.sh

--- app/models/bus.rb
+++ app/models/bus.rb
@@ -1,5 +1,7 @@
 class Bus < ApplicationRecord
   belongs_to :route
+ # has_many :schedules
+ # accepts_nested_attributes_for :schedules
   has_many :tickets, dependent: :destroy
   self.per_page = 12
   default_scope -> { order(created_at: :desc) }
